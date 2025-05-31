3edff2827d6e00fc3f615936db8e1001144a7659
Pachedev <pachequiinn@hotmail.com>
2025-05-30 18:20:44 -0600

    [ADD] Example of using useValue and useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+
+const API_KEY = 'a3c97236-538b-407b-905c-bc01421e4409';
+const API_KEY_PROD = '91ee657f-c0d3-4f9f-b229-0d4560ebf98b';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const request = http.get('https://jsonplaceholder.typicode.com/todos');
+        const task = await lastValueFrom(request);
+        return task.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {}
   getHello(): string {
-    return 'Hello from Store API!';
+    console.log(this.tasks);
+    return `Hello from Store API! ${this.apiKey}`;
   }
 }
