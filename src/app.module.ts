5d0c2b678d883ad6070c9ac3e286ec087fe11198
Pachedev <pachequiinn@hotmail.com>
2025-05-31 09:23:31 -0600

    [ADD] Example of using a Global Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,12 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
-
-const API_KEY = 'a3c97236-538b-407b-905c-bc01421e4409';
-const API_KEY_PROD = '91ee657f-c0d3-4f9f-b229-0d4560ebf98b';
-
+import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [HttpModule, UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
