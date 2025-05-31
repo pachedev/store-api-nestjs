b0895af4b7233b7cf13f21fd193512545a2e01e3
Pachedev <pachequiinn@hotmail.com>
2025-05-31 11:08:23 -0600

    [ADD] Rest API documentation with Swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Store API')
+    .setDescription(
+      'A REST API for a store is a way for other applications to interact with the store´s data and functionality using HTTP requests. It allows for tasks like retrieving product information, managing orders, and updating inventory.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/products/dtos/category.dto.ts
+++ src/products/dtos/category.dto.ts
@@ -1,6 +1,5 @@
 import { IsString, IsNotEmpty } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
-
+import { PartialType } from '@nestjs/swagger';
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
